52a18b49d790c501c415d884d127cb06afac5227
luismelendez94 <2709@holbertonschool.com>
2021-05-14 23:27:13 +0000

    Verifying prints

--- isnum.c
+++ isnum.c
@@ -11,8 +11,16 @@ int _isanum(char *str, int line)
 {
 	int index;
 
+	if (str == NULL)
+	{
+		printf("L%d: usage: push integer\n", line);
+		exit(EXIT_FAILURE);
+	}
+
 	for (index = 0; str[index] != '\0'; index++)
 	{
+		if (str[index] == '-' || str[index] == '0')
+			continue;
 		if (str[index] < '0' || str[index] > '9')
 		{
 			printf("L%d: usage: push integer\n", line);
