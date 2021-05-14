da48899c3501054df39f01230ecd26e44c26b24b
Jose Vazquez <vazqjose@gmail.com>
2021-05-14 02:09:02 +0000

    list of aritmetic funcion prototype

--- monty.h
+++ monty.h
@@ -1,3 +1,14 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -27,3 +38,39 @@ typedef struct instruction_s
         char *opcode;
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
+
+/* DOUBLE LINKED LIST FUNCTIONS BELOW */
+
+stack_t *addEndNode(stack_t **head, const int n);
+stack_t *addnode(stack_t **head, const int n);
+size_t printNode(const stack_t *h);
+void freeList(stack_t *head);
+
+/* LIST MANIPULATION */
+
+stack_t *addEndNode(stack_t **head, const int n);
+stack_t *addnode(stack_t **head, const int n);
+size_t printNode(const stack_t *h);
+void freeList(stack_t *head);
+
+/* FUNCTIONS PER TASK */
+
+int _isanum(char *str, int line);
+void _pall(stack_t **head, unsigned int line);
+void _pint(stack_t **head, unsigned int line);
+void _nop(stack_t **head, unsigned int line);
+void _pop(stack_t **head, unsigned int line);
+void _swap(stack_t **head, unsigned int line);
+void _add(stack_t **head, unsigned int line);
+void _sub(stack_t **head, unsigned int line);
+void _div(stack_t **head, unsigned int line);
+void _mul(stack_t **head, unsigned int line);
+void _mod(stack_t **head, unsigned int line);
+void checkbuffer(FILE *fd, char *buffer, ssize_t _read);
+
+/* GET OP FUNCTION */
+
+void (*opfunc(char *token))(stack_t **head, unsigned int line);
+
+#endif /* MONTY_H */
