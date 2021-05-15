224273abcb0cdf6695b9127471b72cb545a8f3e9
Jose Vazquez <vazqjose@gmail.com>
2021-05-15 01:59:10 +0000

    fix tabs

--- isnum.c
+++ isnum.c
@@ -9,23 +9,23 @@
 
 int _isanum(char *str, int line)
 {
-    int index;
+	int index;
 
-    if (str == NULL)
-    {
-        fprintf(stderr, "L%d: usage: push integer\n", line);
-        exit(EXIT_FAILURE);
+	if (str == NULL)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line);
+		exit(EXIT_FAILURE);
     }
 
-    for (index = 0; str[index] != '\0'; index++)
-    {
-        if (str[index] == '-' || str[index] == '0')
-            continue;
-        if (str[index] < '0' || str[index] > '9')
-        {
-            fprintf(stderr, "L%d: usage: push integer\n", line);
-            exit(EXIT_FAILURE);
-        }
-    }
-    return (0);
+	for (index = 0; str[index] != '\0'; index++)
+	{
+		if (str[index] == '-' || str[index] == '0')
+			continue;
+		if (str[index] < '0' || str[index] > '9')
+		{
+			fprintf(stderr, "L%d: usage: push integer\n", line);
+			exit(EXIT_FAILURE);
+		}
+	}
+	return (0);
 }
