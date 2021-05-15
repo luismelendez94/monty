2e32e47f35b2e6fe41ee4a6bc51da4df3f89244a
Jose Vazquez <vazqjose@gmail.com>
2021-05-15 03:18:29 +0000

    fix

--- nodeFunc2.c
+++ nodeFunc2.c
@@ -8,16 +8,21 @@
 
 void _pchar(stack_t **head, unsigned int line)
 {
-	int n = (*head)->n;
+	int n;
 
-	if (!head || head == NULL)
+	if (head == NULL || *head == NULL)
 	{
 		fprintf(stderr, "L%d: can't pchar, stack empty\n", line);
 		exit(EXIT_FAILURE);
 	}
 
+	n = (*head)->n;
+
 	if (n > 127 || n < 0)
+	{
 		fprintf(stderr, "L%d: can't pchar, value out of range\n", line);
+		exit(EXIT_FAILURE);
+	}
 	else
-		printf("%c\n", toascii(n));
+		printf("%c\n", n);
 }
