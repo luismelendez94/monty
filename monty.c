50ccbe538603b91ca24d377bf6c4d5e7cb62ed06
Jose Vazquez <vazqjose@gmail.com>
2021-05-15 02:19:10 +0000

    fprintf added

--- monty.c
+++ monty.c
@@ -10,25 +10,25 @@
 int main(int argc, char *argv[])
 {
 	FILE *fd;
-/*	ssize_t inputln;	*/
 	char *buffer;
 
 	if (argc != 2)
 	{
-		printf("USAGE: monty file\n");
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 
 	fd = fopen(argv[1], "r");
 	if (fd == NULL)
 	{
-		printf("Error: Can't open file %s\n", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
+
 	buffer = malloc(sizeof(char) * 1024);
 	if (buffer == NULL)
 	{
-		printf("Error: malloc failed\n");
+		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
 
